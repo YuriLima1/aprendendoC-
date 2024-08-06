51e952dd99bb1ffe6cead3013dbf37d157ff2d1e
yuri.aslima <yuri.aslima@ITA0557234W10-1>
2024-08-06 16:04:08 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWord/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Pessoas"); // Escreval
+            Console.Write("Ola Pessoas"); // Escreva
+            Console.ReadKey(); // Leia Tecla
+            Console.Read(); // Leia Até Enter
+        }
+    }
+}
